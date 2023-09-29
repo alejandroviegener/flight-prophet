504937e3e3a73511d7bf532fa970f16d37f255e4
Alejandro Viegener <alejandro.viegener@gmail.com>
2023-09-29 13:14:24 -0300

    added first implementation of model that passes the tests

--- challenge/model.py
+++ challenge/model.py
@@ -1,13 +1,30 @@
 import pandas as pd
 
 from typing import Tuple, Union, List
+from sklearn.linear_model import LogisticRegression
+from datetime import datetime
+import numpy as np
+
+
+FEATURES = [
+    "OPERA_Latin American Wings", 
+    "MES_7",
+    "MES_10",
+    "OPERA_Grupo LATAM",
+    "MES_12",
+    "TIPOVUELO_I",
+    "MES_4",
+    "MES_11",
+    "OPERA_Sky Airline",
+    "OPERA_Copa Air"
+]
 
 class DelayModel:
 
     def __init__(
         self
     ):
-        self._model = None # Model should be saved in this attribute.
+        self._model = LogisticRegression() # Model should be saved in this attribute.
 
     def preprocess(
         self,
@@ -26,7 +43,39 @@ class DelayModel:
             or
             pd.DataFrame: features.
         """
-        return
+
+        # Construct target if required
+        target = None
+        if target_column:
+            data['min_diff'] = data.apply(self._get_min_diff, axis = 1)
+            threshold_in_minutes = 15
+            data[target_column] = np.where(data['min_diff'] > threshold_in_minutes, 1, 0)
+            target = data[[target_column]]
+
+        # Concstruct dummy features
+        data = data[["OPERA", "TIPOVUELO", "MES"]]
+        features = pd.concat(   
+            [
+                pd.get_dummies(data['OPERA'], prefix = 'OPERA'),
+                pd.get_dummies(data['TIPOVUELO'], prefix = 'TIPOVUELO'), 
+                pd.get_dummies(data['MES'], prefix = 'MES')
+            ], 
+            axis = 1
+        )
+
+        # Filter useful features
+        features = features[FEATURES]
+
+        if target_column:   
+            return features, target        
+        return features
+
+    @staticmethod
+    def _get_min_diff(data):
+        fecha_o = datetime.strptime(data['Fecha-O'], '%Y-%m-%d %H:%M:%S')
+        fecha_i = datetime.strptime(data['Fecha-I'], '%Y-%m-%d %H:%M:%S')
+        min_diff = ((fecha_o - fecha_i).total_seconds())/60
+        return min_diff
 
     def fit(
         self,
@@ -40,6 +89,14 @@ class DelayModel:
             features (pd.DataFrame): preprocessed data.
             target (pd.DataFrame): target.
         """
+
+        # Compute class weights and set model parameter accordingly
+        label_0_len = len(target[target['delay'] == 0])
+        label_1_len = len(target[target['delay'] == 1])
+
+        # Fit model
+        self._model.set_params(class_weight={1: label_0_len/len(target), 0: label_1_len/len(target)})
+        self._model.fit(features, target)
         return
 
     def predict(
@@ -55,4 +112,8 @@ class DelayModel:
         Returns:
             (List[int]): predicted targets.
         """
-        return
+
+        # Predict
+        predictions = self._model.predict(features)
+        return predictions.tolist()
+    
---
+++ docs/challenge.md
@@ -0,0 +1,30 @@
+
+Initial scaffolding
+
+
+Docker
+
+Makefile
+
+TODOS:
+- check if problems running are because of windows or not
+
+
+Data science
+
+- Changed notebook, barplot requires paramters for x and y data.
+
+
+Model
+
+- Fixed test in model. Prediction was missing in one of the tests
+- Fixed type hint in model class predict method
+- Changed data path to absolute in tests
+
+TODOS:
+- abstract model into Classifier class
+- implement classifier for LogisticRegression and XGBoost classifier
+
+
+
+
